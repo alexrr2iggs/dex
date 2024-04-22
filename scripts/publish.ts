e8c094d4fe1a9f99e8f06ab6cc83f4c45d957eac
alexrr2iggs <alexandru.bunduche@yahoo.com>
2024-04-22 15:41:29 +0200

    fix

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,6 +1,9 @@
 #!/usr/bin/env -S deno run -A
 
 import { $$, $s } from 'shed/sced.ts';
+import { exeToStdOut } from 'utils/command_execution.ts';
+import * as denoConf from '../deno.json' with { type: 'json' };
+
 $$`deno fmt`
 $$`deno lint`
 $$`deno test -A`
@@ -12,3 +15,7 @@ if (!gitStatus.includes('Your branch is up to date') || !gitStatus.includes('not
     console.log('\nPlease commit your changes before publishing');
     Deno.exit(1);
 }
+
+console.log(`Publishing version `, denoConf.default.version);
+
+exeToStdOut('git', `tag`, `-a v${Deno.env.get('VERSION')}`, `-m "v${Deno.env.get('VERSION')}"`);
