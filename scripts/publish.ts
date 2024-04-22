c0936f4f23a6535140d9410594965149f8889134
alexrr2iggs <alexandru.bunduche@yahoo.com>
2024-04-22 20:37:36 +0200

    "mkreadme"

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S deno run -A
 
 import * as denoConf from '../deno.json' with { type: 'json' };
-import { $$, $s } from '../src/sced.ts';
+import { $$ } from '../src/sced.ts';
 
 const version = denoConf.default.version;
 const tagName = `v${version}`;
@@ -17,16 +17,9 @@ console.log('testing...')
 $$`deno test -A`
 
 
-const gitStatus = $s`git status`
-if (!gitStatus.includes('Your branch is up to date') || !gitStatus.includes('nothing to commit')) {
-    $$`git status`
-
-    console.log('\nPlease commit your changes before publishing');
-
-    Deno.exit(1);
-}
 
 
+$$`deno task repockeck`;
 $$`deno task mkreadme`;
 $$`git commit -a -m "mkreadme"`;
 $$`git push`;
--- scripts/repo_check.ts
+++ scripts/repo_check.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env -S deno run --allow-env --allow-read --allow-run
 import {
     Confirm,
     Input,
