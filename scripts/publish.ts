b88b4a99b39fc152fbf23094f403e626623e9ba6
alexrr2iggs <alexandru.bunduche@yahoo.com>
2024-04-22 19:20:06 +0200

    fx

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S deno run -A
 
 import * as denoConf from '../deno.json' with { type: 'json' };
-import { $$ } from '../src/sced.ts';
+import { $$, $s } from '../src/sced.ts';
 
 const version = denoConf.default.version;
 const tagName = `v${version}`;
@@ -17,14 +17,14 @@ console.log('testing...')
 $$`deno test -A`
 
 
-// const gitStatus = $s`git status`
-// if (!gitStatus.includes('Your branch is up to date') || !gitStatus.includes('nothing to commit')) {
-//     $$`git status`
+const gitStatus = $s`git status`
+if (!gitStatus.includes('Your branch is up to date') || !gitStatus.includes('nothing to commit')) {
+    $$`git status`
 
-//     console.log('\nPlease commit your changes before publishing');
+    console.log('\nPlease commit your changes before publishing');
 
-//     Deno.exit(1);
-// }
+    Deno.exit(1);
+}
 
 
 $$`deno task mkreadme`;
