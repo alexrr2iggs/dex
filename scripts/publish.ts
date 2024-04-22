43c0ed8ba5012a9f22c084b68686959ee14022df
alexrr2iggs <alexandru.bunduche@yahoo.com>
2024-04-22 15:12:09 +0200

    fix

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -12,3 +12,10 @@ if (gitStatus.includes('nothing to commit')) {
 }
 
 
+/**
+ * On branch main
+Your branch is ahead of 'origin/main' by 3 commits.
+  (use "git push" to publish your local commits)
+
+nothing to commit, working tree clean
+ */
